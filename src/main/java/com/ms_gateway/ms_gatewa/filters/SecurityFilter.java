9519d192d78f31074a55e0c86a643c50c71cf74f
Dylan Fernandez <dy.fernandezl@duocuc.cl>
2026-06-26 11:50:16 -0400

    Ultimas configuraciones para que copile

--- src/main/java/com/ms_gateway/ms_gatewa/filters/SecurityFilter.java
+++ src/main/java/com/ms_gateway/ms_gatewa/filters/SecurityFilter.java
@@ -13,26 +13,30 @@ public class SecurityFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         // 0. Obtener la ruta exacta de la petición
+        if (org.springframework.http.HttpMethod.OPTIONS.equals(exchange.getRequest().getMethod())) {
+            return chain.filter(exchange);
+        }
+
         String path = exchange.getRequest().getURI().getPath();
 
         if (path.contains("/swagger-ui") || path.contains("/v3/api-docs")) {
             return chain.filter(exchange);
         }
 
-        // 2. Extrae el token de la cabecera (para las peticiones reales a tu API)
+        // Extrae el token de la cabecera (para las peticiones reales a tu API)
         String token = exchange.getRequest().getHeaders().getFirst("Autorizado");
 
-        // 3. Verifica si existe el token
+        // Verifica si existe el token
         if (token == null || token.isEmpty()) {
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
             return exchange.getResponse().setComplete();
         }
-        // 4. Valida el token (aquí iría tu lógica JWT)
+        // Valida el token (aquí iría tu lógica JWT)
         if (!isValidToken(token)) {
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
             return exchange.getResponse().setComplete();
         }
-        // 5. Si es válido, deja pasar
+        // Si es válido, deja pasar
         return chain.filter(exchange);
     }
 
